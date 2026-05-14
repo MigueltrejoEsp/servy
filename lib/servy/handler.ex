f33a48a9bffdcdc9cfe3776b968d72f722ae15d6
MigueltrejoEsp <mangeel008@gmail.com`>
2026-05-14 14:26:07 -0300

    feat: Handle maps with pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -16,29 +16,51 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method,
+    status: nil,
+    path: path,
+    resp_body: "" }
   end
 
   def route(%{method: "GET", path: "/wildthings"} = conv) do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
   def route(%{method: "GET", path: "/bears"} = conv) do
-    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+    %{ conv |  status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  def route(%{method: "GET", path: "/bigfoot"} = conv) do
-    %{ conv | resp_body: "1, 2, 3" }
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{ conv |  status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
+    %{ conv |  status: 200, resp_body: "Bear #{id} DELETED" }
+  end
+
+  def route(%{path: path, method: _method} = conv) do
+    %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+    200 => "OK",
+    201 => "Created",
+    401 => "Unauthorized",
+    403 => "Forbidden",
+    404 => "Not Found",
+    500 => "Internal Server Error"
+  }[code]
   end
 
 end
@@ -57,6 +79,30 @@ IO.puts response
 
 request = """
 GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+DELETE /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
