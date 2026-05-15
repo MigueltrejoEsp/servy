382b2d0754f603bfb5d118b99a858a64064e82e4
MigueltrejoEsp <mangeel008@gmail.com`>
2026-05-15 14:12:46 -0300

    feat: String concatenation exercise

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,66 +1,81 @@
 defmodule Servy.Handler do
+  @emoji ["😠", "😇", "😄", "😉"]
+
   def handle(request) do
     request
     |> parse
     |> rewrite_path
     |> log
     |> route
+    |> emojify
     |> track
     |> format_response
   end
 
+  defp emojis(), do: Enum.random(@emoji)
+
   def track(%{status: 404, path: path} = conv) do
-    IO.puts "Warning: #{path} is on the loose!"
+    IO.puts("Warning: #{path} is on the loose!")
     conv
   end
 
   def track(conv), do: conv
-  def log(conv), do: IO.inspect conv
+  def log(conv), do: IO.inspect(conv)
 
   def parse(request) do
     [method, path, _] =
       request
       |> String.split("\n")
-      |> List.first
+      |> List.first()
       |> String.split(" ")
 
-    %{ method: method,
-    status: nil,
-    path: path,
-    resp_body: "" }
+    %{method: method, status: nil, path: path, resp_body: ""}
   end
 
   def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{ conv | path: "/wildthings" }
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
+    %{conv | path: "/bears/#{id}"}
   end
 
   def rewrite_path(conv), do: conv
 
   def route(%{method: "GET", path: "/wildthings"} = conv) do
-    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(%{method: "GET", path: "/bears"} = conv) do
-    %{ conv |  status: 200, resp_body: "Teddy, Smokey, Paddington" }
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
   def route(%{method: "GET", path: "/bears/" <> id} = conv) do
-    %{ conv |  status: 200, resp_body: "Bear #{id}" }
+    %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
-    %{ conv |  status: 200, resp_body: "Bear #{id} DELETED" }
+  def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
+    %{conv | status: 403, resp_body: "Bears must never be deleted!"}
   end
 
   def route(%{path: path} = conv) do
-    %{ conv | status: 404, resp_body: "No #{path} here!" }
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
+
+  def emojify(%{status: 200} = conv) do
+    emojies = String.duplicate(emojis(), 5)
+    body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
+
+    %{conv | resp_body: body}
+  end
+
+  def emojify(conv), do: conv
 
   def format_response(conv) do
     """
     HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
-    Content-Length: #{String.length(conv.resp_body)}
+    Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
@@ -68,15 +83,14 @@ defmodule Servy.Handler do
 
   defp status_reason(code) do
     %{
-    200 => "OK",
-    201 => "Created",
-    401 => "Unauthorized",
-    403 => "Forbidden",
-    404 => "Not Found",
-    500 => "Internal Server Error"
-  }[code]
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
-
 end
 
 request = """
@@ -89,7 +103,7 @@ Accept: */*
 
 response = Servy.Handler.handle(request)
 
-IO.puts response
+IO.puts(response)
 
 request = """
 GET /wildthings HTTP/1.1
@@ -101,7 +115,7 @@ Accept: */*
 
 response = Servy.Handler.handle(request)
 
-IO.puts response
+IO.puts(response)
 
 request = """
 GET /bears HTTP/1.1
@@ -113,7 +127,7 @@ Accept: */*
 
 response = Servy.Handler.handle(request)
 
-IO.puts response
+IO.puts(response)
 
 request = """
 GET /bears/1 HTTP/1.1
@@ -125,7 +139,7 @@ Accept: */*
 
 response = Servy.Handler.handle(request)
 
-IO.puts response
+IO.puts(response)
 
 request = """
 DELETE /bears/1 HTTP/1.1
@@ -137,7 +151,19 @@ Accept: */*
 
 response = Servy.Handler.handle(request)
 
-IO.puts response
+IO.puts(response)
+
+request = """
+GET /bears?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
 
 request = """
 GET /bigfoot HTTP/1.1
@@ -149,4 +175,4 @@ Accept: */*
 
 response = Servy.Handler.handle(request)
 
-IO.puts response
+IO.puts(response)
