6b655492c9ceb12bc45add2475403f74e8ca89eb
MigueltrejoEsp <mangeel008@gmail.com`>
2026-08-05 15:52:40 -0300

    using genserver to schedule sending messages

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -47,18 +47,12 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/sensors"} = conv) do
-    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
 
-    snapshots =
-      ["cam-1", "cam-2", "cam-3"]
-      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
-      |> Enum.map(&Task.await/1)
+    sensor_data = Servy.SensorServer.get_sensor_data()
 
-    where_is_bigfoot = Task.await(task)
+    # render(conv, "sensors.eex", snapshots: snapshots, location: where_is_bigfoot)
 
-    render(conv, "sensors.eex", snapshots: snapshots, location: where_is_bigfoot)
-
-    %{conv | status: 200, resp_body: inspect({snapshots, where_is_bigfoot})}
+    %{conv | status: 200, resp_body: inspect(sensor_data)}
   end
 
   def route(%Conv{method: "GET", path: "/kaboom"}) do
