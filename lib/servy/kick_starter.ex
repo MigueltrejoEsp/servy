017824be3c8cd72734eb7359b4703283769ae2fe
MigueltrejoEsp <mangeel008@gmail.com`>
2026-08-10 16:06:30 -0300

    fix

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -1,10 +1,18 @@
 defmodule Servy.KickStarter do
   use GenServer
 
+  # Client Interface
+
   def start do
-    IO.puts("Starting the KickStarter...")
+    IO.puts("Starting the kickstarter...")
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
+
+  def get_server do
+    GenServer.call(__MODULE__, :get_server)
+  end
+
+  # Server Callbacks
 
   def init(:ok) do
     Process.flag(:trap_exit, true)
@@ -12,17 +20,18 @@ defmodule Servy.KickStarter do
     {:ok, server_pid}
   end
 
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
+  end
+
   def handle_info({:EXIT, _pid, reason}, _state) do
-    IO.puts("HttpServer exited #{inspect(reason)}")
+    IO.puts("HttpServer exited (#{inspect(reason)})")
     server_pid = start_server()
     {:noreply, server_pid}
   end
 
-  def start_server do
-    IO.puts("Starting the HTTPServer...")
-
-    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
-    Process.register(server_pid, :http_server)
-    server_pid
+  defp start_server do
+    IO.puts("Starting the HTTP server...")
+    spawn_link(Servy.HttpServer, :start, [4000])
   end
 end
