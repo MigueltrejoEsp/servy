c45162549c208224a582035dab4ec1e7868e72a1
MigueltrejoEsp <mangeel008@gmail.com`>
2026-08-11 09:25:48 -0300

    OTP Supervison tree

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -1,14 +1,14 @@
-defmodule Servy.ServicesSupervisor do
+defmodule Servy.Supervisor do
   use Supervisor
 
-  def start_link(_arg) do
-    IO.puts("Starting the services supervisor...")
+  def start_link do
+    IO.puts("Starting THE supervisor...")
     Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
     children = [
-      Servy.PledgeServer,
+      Servy.KickStarter,
       Servy.SensorServer
     ]
 
